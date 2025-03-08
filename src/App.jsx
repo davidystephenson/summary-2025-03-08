c55a3b30c85804f22cec3795b50e05f51c0e6c2a
David Y. Stephenson <david@davidystephenson.com>
2025-03-08 11:49:18 -0500

    Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hello from "./Hello"
 import Count from "./Count"
+import Form from "./Form"
 
 export default function App () {
   return (
     <div id='wrapper'>
+      <Form />
       <Count />
       <Hello person='Dorothy' />
       <Hello person='Zelda' />
---
+++ src/Form.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function Form () {
+  const [answer, setAnswer] = useState('')
+  function handleChange (event) {
+    setAnswer(event.target.value)
+  }
+  function handleSubmit (event) {
+    event.preventDefault()
+    const correct = answer === '2'
+    console.log('correct', correct)
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>1 + 1 = ???</p>
+      <input value={answer} onChange={handleChange} />
+      <button>Submit</button>
+    </form>
+  )
+}
