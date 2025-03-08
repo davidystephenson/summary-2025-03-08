88bd9476f1aaaeaf91c02fd68da9fb198070883c
David Y. Stephenson <david@davidystephenson.com>
2025-03-08 11:14:35 -0500

    Goodbye component

---
+++ src/Goodbye.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export default class Goodbye extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>Goodbye?</div>
+    )
+  }
+}
