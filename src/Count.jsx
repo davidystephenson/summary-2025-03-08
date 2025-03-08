083cd3b814afe2e1d3ca43588262e0156369c06e
David Y. Stephenson <david@davidystephenson.com>
2025-03-08 11:43:24 -0500

    Count component with state

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
+// let count = 10
+
 export default function Count () {
+  const [count, setCount] = useState(1)
+  console.log('setCount', setCount)
   function handleClick () {
     console.log('click')
+    // count += 1
+    setCount(count + 1)
+    console.log('count', count)
   }
   return (
-    <button onClick={handleClick}>Count: 0</button>
+    <button onClick={handleClick}>Count: {count}</button>
   )
 }
